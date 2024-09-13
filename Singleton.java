482d1910efdb31ce1661ecedec600d93f919b399
EWONDJO JOSEPH <ewondjojoseph@gmail.com>
2024-09-13 04:51:07 +0100

    projet.java

--- Singleton.java
+++ Singleton.java
@@ -1,43 +1,73 @@
-package construction.singleton;
 
-public final class Singleton {
-    private static Singleton instance =null;
+ final class MSingleton {
+    private static MSingleton instance = null;
 
     private int x;
     private int y;
 
-    private Singleton()
+    private MSingleton()
     {
         super();
     }
 
-    private Singleton (int x, int y)
+    private MSingleton (int x, int y)
     {
         this.x=x;
         this.y=y;
     }
 
-    public static Singleton getInstance ()
+    public static MSingleton getInstance ()
     {
         if (instance == null)
         {
-            instance = new Singleton();
+            instance = new MSingleton();
         }
         return instance;
     }
 
-    public static Singleton getInstance(int x,int y)
+    public static MSingleton getInstance(int x,int y)
     {
         if (instance == null)
         {
-            instance = new Singleton(x,y);
+            instance = new MSingleton(x,y);
         }
         return instance;
     }
 
+    public int somme(int x,int y){
+        return x+y;
+    }
 
+    public float moyenne(int x , int y)
+    {
+        return somme(x,y)/2;
+    }
 
+    public void affiche()
+    {
+        System.out.println("\n je suis une instance mes valeurs sont : x = "+this.x+" et y = "+this.y);
+    }
 
+    @Override 
+    public Object clone() throws
+    CloneNotSupportedException{
+        throw new CloneNotSupportedException();
+    }
+
+}
+
+public class Singleton{
+    public static void main (String[] args)
+    {
+        int som = MSingleton.getInstance().somme(2,5);
+        System.out.printf("La somme est : %d",som);
+
+        MSingleton s1 = MSingleton.getInstance(8, 3);
+        s1.affiche();
+        MSingleton s2= MSingleton.getInstance(5, 9);
+        s2.affiche();
+
+            }
 }
 
 /*
